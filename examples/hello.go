3b7f4dedbca9416a0715438cad1e40114d2eeb75
Scott Lawrence <bytbox@gmail.com>
2010-07-23 13:59:03 -0400

    'Hello, world' example now print out list of arguments.

--- examples/hello.go
+++ examples/hello.go
@@ -9,4 +9,7 @@ func main() {
 	opts.Shortopt("h", "get help", "false")
 	opts.Parse()
 	fmt.Printf("Xname: %s\n", opts.Xname)
+	for i := 0; i < len(opts.Args); i++ {
+		fmt.Printf("Argument: %s\n", opts.Args[i])
+	}
 }
